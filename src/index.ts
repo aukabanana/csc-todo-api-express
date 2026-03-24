2f4d947616184209f3edf68c88b85a7bac465a88
ratchapol <hope25501@gmail.com>
2026-03-24 16:50:04 +0700

    feat: Basic express setup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express, {request,response} from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
